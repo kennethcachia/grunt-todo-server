22882702897f8589952561520e53e68ef01b38a5
Kenneth Cachia <kcachia@gmail.com>
2014-02-17 19:18:12 -0500

    Cleanup.

--- tasks/todo_server.js
+++ tasks/todo_server.js
@@ -75,7 +75,7 @@ module.exports = function(grunt) {
       while ((match = regex.exec(raw)) !== null) {
 
         if (match[2]) {
-          match[2] = match[2].trim()
+          match[2] = match[2].trim();
         }
 
         key.push({
